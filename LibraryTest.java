514a26b59fbab0ff4295c82af8c76a25e2fd3410
Juanita-Christene Adigwe <g4gmh@remotelabm38.cs.unb.ca>
2024-03-30 09:40:52 -0300

    Library Test to add and remove books and memebers from a libray

---
+++ LibraryTest.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void testAddBook() {
+        Library library = new Library();
+        try {
+		Book book = new Book("1", "Book Title", "Author");
+		library.addBook(book);
+        } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+        }
+        assertTrue(library.searchBooksByTitle("Book Title").contains(book));
+    }
+
+    @Test
+    public void testRemoveBook() {
+        Library library = new Library();
+        try {
+		Book book = new Book("1", "Book Title", "Author");
+        	library.addBook(book);
+                library.removeBook(book);
+        } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+        }
+        assertFalse(library.searchBooksByTitle("Book Title").contains(book));
+    }
+
+    @Test
+    public void testAddMember() {
+        Library library = new Library();
+        try {
+		Member member = new Member("1", "John Doe", "123456789", "123 Main St");
+		library.addMember(member);
+        } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+        }
+        assertEquals(member, library.findMemberById("1"));
+    }
+
+    @Test
+    public void testRemoveMember() {
+        Library library = new Library();
+        try {
+		Member member = new Member("1", "John Doe", "123456789", "123 Main St");
+		library.addMember(member);
+		library.removeMember("1");
+        } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+        }
+        assertNull(library.findMemberById("1"))  
+    }
+
+}
